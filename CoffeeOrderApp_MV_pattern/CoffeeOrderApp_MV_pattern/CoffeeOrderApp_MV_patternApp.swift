65e7d87644dc8a8f613700220b7b2368c68845e6
Salman Khan <salmaniOSdev123@gmail.com>
2024-06-11 18:22:53 +0530

    Initial setup

---
+++ CoffeeOrderApp_MV_pattern/CoffeeOrderApp_MV_pattern/CoffeeOrderApp_MV_patternApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeOrderApp_MV_patternApp.swift
+//  CoffeeOrderApp_MV_pattern
+//
+//  Created by Salman_Macbook on 11/06/24.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeOrderApp_MV_patternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoffeeOrderApp_MV_pattern/CoffeeOrderApp_MV_pattern/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoffeeOrderApp_MV_pattern
+//
+//  Created by Salman_Macbook on 11/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
