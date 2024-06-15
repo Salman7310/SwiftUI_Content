341a2cdb4fd1df74a013b678e200a6fa57cf0eed
Salman Khan <salmaniOSdev123@gmail.com>
2024-06-15 12:21:57 +0530

    Completed end to end testing for update order

--- CoffeeOrderApp_MV_pattern/CoffeeOrderAppE2ETests/CoffeeOrderAppE2ETests.swift
+++ CoffeeOrderApp_MV_pattern/CoffeeOrderAppE2ETests/CoffeeOrderAppE2ETests.swift
@@ -7,6 +7,66 @@
 
 import XCTest
 
+final class when_app_is_launched_with_no_orders: XCTestCase {
+
+    func test_should_make_sure_no_orders_message_is_displayed() {
+        let app = XCUIApplication()
+        continueAfterFailure = false
+        app.launchEnvironment = ["ENV": "TEST"]
+        app.launch()
+        
+        XCTAssertEqual("No orders available!", app.staticTexts["noOrdersText"].label)
+    }
+
+}
+
+final class when_adding_a_new_coffee_order: XCTestCase {
+    
+    private var app: XCUIApplication!
+    
+    // called before running each test
+    override func setUp() {
+        app = XCUIApplication()
+        continueAfterFailure = false
+        app.launchEnvironment = ["ENV": "TEST"]
+        app.launch()
+        
+        // go to place order screen
+        app.buttons["addNewOrderButton"].tap()
+        // fill out the textfields
+        let nameTextField = app.textFields["name"]
+        let coffeeNameTextField = app.textFields["coffeeName"]
+        let priceTextField = app.textFields["price"]
+        let placeOrderButton = app.buttons["placeOrderButton"]
+        
+        nameTextField.tap()
+        nameTextField.typeText("Abhishek")
+        
+        coffeeNameTextField.tap()
+        coffeeNameTextField.typeText("Hot Coffee")
+        
+        priceTextField.tap()
+        priceTextField.typeText("4.50")
+        
+        // place the order
+        placeOrderButton.tap()
+    }
+    
+    func test_should_display_coffee_order_in_list_successfully() throws {
+        XCTAssertEqual("Abhishek", app.staticTexts["orderNameText"].label)
+        XCTAssertEqual("Hot Coffee (Medium)", app.staticTexts["coffeeNameAndSizeText"].label)
+        //XCTAssertEqual("₹ 4.50", app.staticTexts["coffeePriceText"].label)
+    }
+    
+    // called after running each test
+    override func tearDown() {
+        Task {
+            guard let url = URL(string: "/test/clear-orders", relativeTo: URL(string: "https://island-bramble.glitch.me")!) else { return }
+            let (_, _) = try! await URLSession.shared.data(from: url)
+        }
+    }
+}
+
 final class when_deleting_an_order: XCTestCase {
     
     private var app: XCUIApplication!
@@ -58,27 +118,27 @@ final class when_deleting_an_order: XCTestCase {
     }
 }
 
-final class when_adding_a_new_coffee_order: XCTestCase {
+final class when_updating_an_existing_order: XCTestCase {
     
     private var app: XCUIApplication!
     
-    // called before running each test
     override func setUp() {
         app = XCUIApplication()
         continueAfterFailure = false
         app.launchEnvironment = ["ENV": "TEST"]
         app.launch()
         
-        // go to place order screen
+        // go to the add order screen
         app.buttons["addNewOrderButton"].tap()
-        // fill out the textfields
+        
+        // write into textfields
         let nameTextField = app.textFields["name"]
         let coffeeNameTextField = app.textFields["coffeeName"]
         let priceTextField = app.textFields["price"]
         let placeOrderButton = app.buttons["placeOrderButton"]
         
         nameTextField.tap()
-        nameTextField.typeText("Abhishek")
+        nameTextField.typeText("John")
         
         coffeeNameTextField.tap()
         coffeeNameTextField.typeText("Hot Coffee")
@@ -90,30 +150,49 @@ final class when_adding_a_new_coffee_order: XCTestCase {
         placeOrderButton.tap()
     }
     
-    func test_should_display_coffee_order_in_list_successfully() throws {
-        XCTAssertEqual("Abhishek", app.staticTexts["orderNameText"].label)
-        XCTAssertEqual("Hot Coffee (Medium)", app.staticTexts["coffeeNameAndSizeText"].label)
-        //XCTAssertEqual("₹ 4.50", app.staticTexts["coffeePriceText"].label)
+    func test_should_update_order_successfully() {
+        
+        // go to the order screen
+        let orderList = app.collectionViews["orderList"]
+        orderList.buttons["orderNameText-coffeeNameAndSizeText-coffeePriceText"].tap()
+        
+        app.buttons["editOrderButton"].tap()
+        
+        let nameTextField = app.textFields["name"]
+        let coffeeNameTextField = app.textFields["coffeeName"]
+        let priceTextField = app.textFields["price"]
+        let placeOrderButton = app.buttons["placeOrderButton"]
+        
+        let _ = nameTextField.waitForExistence(timeout: 2.0)
+        nameTextField.tap(withNumberOfTaps: 2, numberOfTouches: 1)
+        nameTextField.typeText("John Edit")
+        
+        
+        let app = XCUIApplication()
+        app.collectionViews["orderList"]/*@START_MENU_TOKEN@*/.buttons["orderNameText-coffeeNameAndSizeText-coffeePriceText"]/*[[".cells",".buttons[\"John Edit, Hot Coffee Edit (Medium), ₹ 1.50\"]",".buttons[\"orderNameText-coffeeNameAndSizeText-coffeePriceText\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app/*@START_MENU_TOKEN@*/.buttons["editOrderButton"]/*[[".buttons[\"Edit Order\"]",".buttons[\"editOrderButton\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        let collectionViewsQuery = app.collectionViews
+        collectionViewsQuery/*@START_MENU_TOKEN@*/.textFields["coffeeName"]/*[[".cells",".textFields[\"Coffee name\"]",".textFields[\"coffeeName\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.tap()
+        collectionViewsQuery/*@START_MENU_TOKEN@*/.buttons["placeOrderButton"]/*[[".cells",".buttons[\"Update Order\"]",".buttons[\"placeOrderButton\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.tap()
+        let _ = coffeeNameTextField.waitForExistence(timeout: 2.0)
+        coffeeNameTextField.tap(withNumberOfTaps: 2, numberOfTouches: 1)
+        coffeeNameTextField.typeText("Hot Coffee Edit")
+        
+        let _ = priceTextField.waitForExistence(timeout: 2.0)
+        priceTextField.tap(withNumberOfTaps: 2, numberOfTouches: 1)
+        priceTextField.typeText("1.50")
+        
+        placeOrderButton.tap()
+        
+        XCTAssertEqual("Hot Coffee Edit", app.staticTexts["coffeeNameText"].label)
     }
     
-    // called after running each test
+    // TEAR DOWN FUNCTIONS RUNS AND THEN DELETE ALL ORDERS FROM THE TEST DATABASE
     override func tearDown() {
         Task {
             guard let url = URL(string: "/test/clear-orders", relativeTo: URL(string: "https://island-bramble.glitch.me")!) else { return }
             let (_, _) = try! await URLSession.shared.data(from: url)
         }
     }
-}
-
-final class when_app_is_launched_with_no_orders: XCTestCase {
-
-    func test_should_make_sure_no_orders_message_is_displayed() {
-        let app = XCUIApplication()
-        continueAfterFailure = false
-        app.launchEnvironment = ["ENV": "TEST"]
-        app.launch()
-        
-        XCTAssertEqual("No orders available!", app.staticTexts["noOrdersText"].label)
-    }
-    
 }
--- CoffeeOrderApp_MV_pattern/CoffeeOrderApp_MV_pattern/ContentView.swift
+++ CoffeeOrderApp_MV_pattern/CoffeeOrderApp_MV_pattern/ContentView.swift
@@ -50,6 +50,7 @@ struct ContentView: View {
                             }
                         }.onDelete(perform: deleteOrder)
                     }
+                    .accessibilityIdentifier("orderList")
                 }
             }
             .navigationDestination(for: Int.self, destination: { orderId in
