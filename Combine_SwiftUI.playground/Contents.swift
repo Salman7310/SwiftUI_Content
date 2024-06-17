d926bb5a65e58d7ea7e83d5136388e384c45234c
Salman Khan <salmaniOSdev123@gmail.com>
2024-06-17 11:52:47 +0530

    Completed Timer publishers

---
+++ Combine_SwiftUI.playground/Contents.swift
@@ -0,0 +1,31 @@
+import UIKit
+import Combine
+
+// Creating and working with Publisher:-
+let publisher = Just("Hello World")
+
+let cancellable = publisher.sink { value in
+    print(value)
+}
+
+// cancel is not required it will cancel automatically once it goes out of the scope.
+cancellable.cancel()
+
+
+// Sequence Publisher
+
+let numberPublisher = [1,2,3,4,5,6].publisher
+let doublePublisher = numberPublisher.map { $0 * 2 }
+
+doublePublisher.sink { value in
+    print(value)
+}
+
+
+// Subscribing to Publishers
+// Timer Publisher
+
+let timerPublisher = Timer.publish(every: 1, on: .main, in: .common)
+let cancelable = timerPublisher.autoconnect().sink { timestamp in
+    print("Timestamp:- \(timestamp)")
+}
