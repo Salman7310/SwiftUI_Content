4cd2fc62e9822fb37f426a69f8d0afedfa6b93df
Salman Khan <salmaniOSdev123@gmail.com>
2024-06-17 13:51:33 +0530

    Done Subscription Lifecycle

---
+++ Subscription_Lifecycle/Subscription_Lifecycle/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  Subscription_Lifecycle
+//
+//  Created by Salman_Macbook on 17/06/24.
+//
+
+import SwiftUI
+import Combine
+
+class ContentViewModel: ObservableObject {
+    
+    @Published var value: Int = 0
+    var cancellable: AnyCancellable?
+    
+    init() {
+        let publisher = Timer.publish(every: 1.0, on: .main, in: .default)
+            .autoconnect()
+            .map {
+                _  in self.value + 1
+            }
+        
+        cancellable = publisher.assign(to: \.value, on: self)
+    }
+}
+
+struct ContentView: View {
+    
+    @StateObject private var vm = ContentViewModel()
+    
+    var body: some View {
+        VStack {
+            Text("\(vm.value)")
+                .font(.largeTitle)
+        }
+        .onDisappear(perform: {
+            vm.cancellable?.cancel()
+        })
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Subscription_Lifecycle/Subscription_Lifecycle/Subscription_LifecycleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Subscription_LifecycleApp.swift
+//  Subscription_Lifecycle
+//
+//  Created by Salman_Macbook on 17/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Subscription_LifecycleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
