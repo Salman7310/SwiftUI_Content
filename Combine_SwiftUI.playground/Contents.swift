359f94d940793b18baef03ea470d0b6e1be982e9
Salman Khan <salmaniOSdev123@gmail.com>
2024-06-17 16:35:05 +0530

    Error Handeling in Combine

--- Combine_SwiftUI.playground/Contents.swift
+++ Combine_SwiftUI.playground/Contents.swift
@@ -1,7 +1,7 @@
 import UIKit
 import Combine
 
-// Creating and working with Publisher:-
+/*// Creating and working with Publisher:-
 let publisher = Just("Hello World")
 
 let cancellable = publisher.sink { value in
@@ -41,4 +41,68 @@ let cancellables = numberPublishers.sink { value in
 DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
     cancellables.cancel()
     print("Subscription Cancelled")
+}*/
+
+
+// Error Handeling in Combine
+
+enum NumberError: Error {
+    case operationFailed
 }
+
+/*let numberPublisher = [1,2,3,4,5,6].publisher
+
+let doublePublisher = numberPublisher.tryMap { number in
+    if number == 4 {
+        throw NumberError.operationFailed
+    }
+    
+    return number * 2
+}
+    .catch { error in
+        if let numberError = error as? NumberError {
+            print("Error occurred: \(numberError)")
+        }
+        
+        return Just(0) // Return is mendatory in catch, that's why returned Just(0)
+    }
+
+// Now Subscribe the Publisher
+let cancellable = doublePublisher.sink { completion in
+    switch completion {
+    case .finished:
+        print("finished")
+    case .failure(let error):
+        print(error)
+    }
+} receiveValue: { value in
+    print(value)
+}*/
+
+
+// Handle same error by using mapError
+let numberPublisher = [1,2,3,4,5,6].publisher
+
+let doublePublisher = numberPublisher.tryMap { number in
+    if number == 4 {
+        throw NumberError.operationFailed
+    }
+    
+    return number * 2
+}
+    .mapError { error in
+        return NumberError.operationFailed
+    }
+
+
+let cancellable = doublePublisher.sink { completion in
+    switch completion {
+    case .finished:
+        print("Finished")
+    case .failure(let error):
+        print(error)
+    }
+} receiveValue: { value in
+    print(value)
+}
+
