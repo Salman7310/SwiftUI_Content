a5add73248b4abf39dd69c4174e54a4e2dee5dc8
Salman Khan <salmaniOSdev123@gmail.com>
2024-06-19 18:03:38 +0530

    Initial setup completed

---
+++ MovieAppUIKit+Combine/MovieAppUIKit+Combine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieAppUIKit+Combine
+//
+//  Created by Salman_Macbook on 19/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
