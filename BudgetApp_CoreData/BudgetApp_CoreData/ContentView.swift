c2281834265d2afe87411b62a961f1d24a76341c
Salman Khan <salmaniOSdev123@gmail.com>
2024-06-24 14:21:43 +0530

    Showing list of added budget

--- BudgetApp_CoreData/BudgetApp_CoreData/ContentView.swift
+++ BudgetApp_CoreData/BudgetApp_CoreData/ContentView.swift
@@ -6,16 +6,33 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct ContentView: View {
+    
+    @Environment(\.managedObjectContext) private var viewContext
+    @FetchRequest(sortDescriptors: []) private var budgetCategoryResults: FetchedResults<BudgetCategory>
+    @State private var isPresented: Bool = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                List(budgetCategoryResults) { budgetCategory in
+                    Text(budgetCategory.title ?? "")
+                }
+            }
+            .sheet(isPresented: $isPresented, content: {
+                AddBudgetCategoryView()
+            })
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Add Category") {
+                        isPresented = true
+                    }
+                }
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
