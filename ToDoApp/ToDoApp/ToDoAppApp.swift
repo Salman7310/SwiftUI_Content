49dfc4ba7609a221fdb4dc53c9e848e0ff4f3998
Salman Khan <salmaniOSdev123@gmail.com>
2024-07-04 13:20:53 +0530

    Todo App initial code

---
+++ ToDoApp/ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Salman_Macbook on 04/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
