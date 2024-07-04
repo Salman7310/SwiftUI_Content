b1146ed90f188f7f69a2cdcb1bc1e57d03cc59b4
Salman Khan <salmaniOSdev123@gmail.com>
2024-07-04 13:51:46 +0530

    Implementing core data provider

---
+++ ToDoApp/ToDoApp/Providers/CoreDataProvider.swift
@@ -0,0 +1,54 @@
+//
+//  CoreDataProvider.swift
+//  ToDoApp
+//
+//  Created by Salman_Macbook on 04/07/24.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataProvider {
+    
+    let persistentContainer: NSPersistentContainer
+    
+    var viewContext: NSManagedObjectContext {
+        persistentContainer.viewContext
+    }
+    
+    init(inMemory: Bool = false) {
+        
+        persistentContainer = NSPersistentContainer(name: "TodoModel")
+        
+        if inMemory {
+            persistentContainer.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+        }
+        
+        persistentContainer.loadPersistentStores { _, error in
+            if let error {
+                fatalError("Core Data store failed to initialize \(error)")
+            }
+        }
+    }
+    
+    // create the preview
+    static var preview: CoreDataProvider {
+        
+        let provider = CoreDataProvider(inMemory: true)
+        let viewContext = provider.viewContext
+        
+        // put some dummy data because when coredata provider initilizes it will load the data and if data is nill then app will be crashing.
+        for index in 1..<10 {
+            let todoItem = TodoItem(context: viewContext)
+            todoItem.title = "todoItem \(index)"
+        }
+        
+        do {
+            try viewContext.save()
+        } catch {
+            print(error)
+        }
+        
+        return provider
+    }
+}
