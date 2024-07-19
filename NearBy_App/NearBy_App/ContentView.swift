61ea630e0f2a8be0f1de2d63be3c6c3254e767de
Salman Khan <salmaniOSdev123@gmail.com>
2024-07-19 14:57:23 +0530

    Started working on NearBy App

---
+++ NearBy_App/NearBy_App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NearBy_App
+//
+//  Created by Salman_Macbook on 19/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NearBy_App/NearBy_App/NearBy_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearBy_AppApp.swift
+//  NearBy_App
+//
+//  Created by Salman_Macbook on 19/07/24.
+//
+
+import SwiftUI
+
+@main
+struct NearBy_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
